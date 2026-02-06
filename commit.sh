c96f06c7feec5ccddf5f600a50fc7dcf2bf68676
rameezr10 <rameez@Rameez-PC.>
2026-02-06 17:25:49 +0500

    Cloud Computing Assignment 1

---
+++ commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Error: Commit message is missing."
+    exit 1
+fi
+
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    echo "Error: Current directory is not a Git repository."
+    exit 1
+fi
+
+git add .
+git commit -m "$1"
+git push
+
+echo "Success: Changes have been staged, committed, and pushed."
